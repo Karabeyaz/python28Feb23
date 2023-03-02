04ba32c18e0343bf98c53d7f44705a9d308ba824
Haq <nabilmahmud2031@gmail.com>
2023-03-01 20:58:40 -0500

    more on variables

--- variable.py
+++ variable.py
@@ -1,4 +1,10 @@
+# Multiple variables in one line
 x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+# One value to multiple variables
+x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
