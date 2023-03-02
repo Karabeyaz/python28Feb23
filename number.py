1f4320cf7d9289fb90cc52e5fa2ce209ce6c3c7b
Haq <nabilmahmud2031@gmail.com>
2023-03-01 21:14:26 -0500

    type conversion

---
+++ number.py
@@ -0,0 +1,15 @@
+# there are three numeric types in python
+x = 1 #int
+y = 2.8 #float
+z = 1j #complex
+
+print(type(x))
+
+#type conversion
+x = 25
+#convert from int to float
+x_float = float(x)
+print(x_float)
+#convert from float to complex
+x_complex = complex(x_float)
+print(x_complex)
